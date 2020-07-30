90adc8ae21ad103e56b2c886742425a52eab79e0
Marlon <luciousmc@icloud.com>
2020-07-30 11:18:07 -0700

    adjusts position of menu button changes menu button position to sticky

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const menuBtn = document.getElementById('menu-btn');
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 
 // click handler to show/hide menu
 menuBtn.addEventListener('click', event => {
   menu.classList.toggle('open');
+  menuBtn.classList.toggle('x');
 });
