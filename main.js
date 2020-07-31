dc56197263a31cc4cc254cef47597dc458f44a31
Marlon <luciousmc@icloud.com>
2020-07-31 11:40:31 -0700

    completes menu button animation

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtnContainer = document.querySelector('#menu-btn-container');
+const menuBtn = document.querySelector('.menu-btn')
 const menu = document.querySelector('.menu');
 
 // click handler to show/hide menu
-menuBtn.addEventListener('click', event => {
+menuBtnContainer.addEventListener('click', event => {
   menu.classList.toggle('open');
   menuBtn.classList.toggle('x');
 });
